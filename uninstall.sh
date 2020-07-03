93c3e03b02ee861360c375865b595e579b4ec0e7
littleforce <32984811+littleforce163@users.noreply.github.com>
2020-07-03 16:14:39 +0800

    uninstall k3s

---
+++ uninstall.sh
@@ -0,0 +1,5 @@
+sudo bash /usr/local/bin/k3s-killall.sh
+docker stop `docker ps |awk {'print $1'}|grep -v CONTAINER`
+docker rm -f $(docker ps -qa)
+docker volume rm $(docker volume ls -q)
+sudo bash /usr/local/bin/k3s-uninstall.sh
