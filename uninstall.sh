0ffb8c3b2ce3ecdcbe71d4017ce49878b2a24992
littleforce <32984811+littleforce163@users.noreply.github.com>
2020-07-03 16:17:05 +0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -3,3 +3,4 @@ docker stop `docker ps |awk {'print $1'}|grep -v CONTAINER`
 docker rm -f $(docker ps -qa)
 docker volume rm $(docker volume ls -q)
 sudo bash /usr/local/bin/k3s-uninstall.sh
+sudo modprobe -r ipip
